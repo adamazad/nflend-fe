ed7fcaefe8d1bd2e3257007d8afdf7da612cc6d1
Adam Azad <adam@adamazad.com>
2021-01-29 17:07:23 +0300

    Fix content margin

--- src/layouts/HeaderAndContent.tsx
+++ src/layouts/HeaderAndContent.tsx
@@ -1,5 +1,4 @@
 // Externals
-import { useTheme } from 'styled-components'
 import React from 'react'
 
 // Components
@@ -7,12 +6,10 @@ import { Header } from 'src/components/Header'
 import { Flex } from 'src/components/Flex'
 
 export const HeaderAndContent: React.FC = ({ children }) => {
-  const { header } = useTheme()
-
   return (
     <Flex flexDirection="column" minHeight="100%">
       <Header />
-      <Flex marginTop={header.height} py={20} flexGrow={3}>
+      <Flex py={20} flexGrow={3}>
         {children}
       </Flex>
     </Flex>
