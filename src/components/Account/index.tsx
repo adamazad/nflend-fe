112173c6caa3a76d0ece43f59e860c5fcb885b65
Adam Azad <adam@adamazad.com>
2021-01-30 22:38:55 +0300

    Update style

--- src/components/Account/index.tsx
+++ src/components/Account/index.tsx
@@ -2,6 +2,7 @@
 import { InjectedConnector } from '@web3-react/injected-connector'
 import { useWeb3React } from '@web3-react/core'
 import React, { useCallback } from 'react'
+import styled from 'styled-components'
 
 // Wallet helpers
 import { getErrorMessage } from 'src/wallet'
@@ -9,26 +10,36 @@ import { getErrorMessage } from 'src/wallet'
 // Components
 import { Button } from '../Button'
 
+const ConnectButton = styled(Button)({
+  border: 'none',
+  fontWeight: 'bold',
+  background: 'transparent',
+})
+
+const AccountText = styled.div({
+  fontWeight: 'bold',
+})
+
 export function Account() {
   const { activate, account } = useWeb3React()
 
   const connectWallet = useCallback(() => {
     activate(new InjectedConnector({})).catch(error => {
-      console.log(getErrorMessage(error))
+      alert(getErrorMessage(error))
     })
   }, [activate])
 
   if (account) {
     return (
-      <div>
+      <AccountText>
         {account.slice(0, 3)}...{account.slice(account.length - 3)}
-      </div>
+      </AccountText>
     )
   }
 
   return (
     <div>
-      <Button onClick={connectWallet}>Login</Button>
+      <ConnectButton onClick={connectWallet}>Login</ConnectButton>
     </div>
   )
 }
