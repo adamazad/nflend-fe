06046e3879e3bf09b901106e8b2024ebaed31649
Adam Azad <adam@adamazad.com>
2021-01-31 23:33:52 +0300

    Add more routes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,8 +1,10 @@
 // External
 import { Route, Switch } from 'react-router-dom'
-import React from 'react'
 
 // Views
+import { CreateBorrowRequestView } from 'src/views/CreateBorrowRequest'
+import { BorrowRequestsView } from 'src/views/BorrowRequests'
+import { DepisteNFTView } from 'src/views/DepositNFT'
 import { NotFoundView } from 'src/views/NotFound'
 import { ConnectView } from 'src/views/Connect'
 import { LandingView } from 'src/views/Landing'
@@ -15,6 +17,9 @@ export const AppRouter = () => {
       <Route exact path="/connect" component={ConnectView} />
       <Route exact path="/borrow" component={BorrowView} />
       <Route exact path="/lend" component={LendView} />
+      <Route exact path="/assets/deposit" component={DepisteNFTView} />
+      <Route exact path="/borrow-requests" component={BorrowRequestsView} />
+      <Route exact path="/borrow-requests/new" component={CreateBorrowRequestView} />
       <Route exact path="/" component={LandingView} />
       <Route exact path="*" component={NotFoundView} />
     </Switch>
