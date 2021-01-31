e8496347c14a0d15115a266788c7ad73d8360545
Adam Azad <adam@adamazad.com>
2021-01-31 11:24:29 +0300

    Add landing view

--- src/views/Landing/index.tsx
+++ src/views/Landing/index.tsx
@@ -1,16 +1,60 @@
 // Externals
-import React from 'react'
+import { useHistory } from 'react-router-dom'
+import styled from 'styled-components'
+import { useCallback } from 'react'
 
 // Layouts
 import { HeaderAndContent } from 'src/layouts/HeaderAndContent'
 import { Center } from 'src/layouts/Center'
 
+// Hooks
+import { useWalletConnect } from 'src/hooks/useWalletConnect'
+
+// Components
+import { Container } from 'src/components/Container'
+import { Button } from 'src/components/Button'
+
 export function LandingView() {
+  const { activate } = useWalletConnect()
+  const history = useHistory()
+
+  const onClickBorrow = useCallback(() => {
+    activate()
+      .then(() => history.push('/borrow-requests/new'))
+      .catch(error => {
+        alert(error)
+      })
+  }, [activate, history])
+
   return (
     <HeaderAndContent>
-      <Center minHeight="100%">
-        <div>Take a loan against that NFT!</div>
-      </Center>
+      <Container>
+        <Center minHeight="100%" minWidth="100%">
+          <Heading>Leverage your NFTs</Heading>
+          <BorrowButton onClick={onClickBorrow}>Open a Loan</BorrowButton>
+        </Center>
+      </Container>
     </HeaderAndContent>
   )
 }
+
+const Heading = styled.h1`
+  text-align: center;
+  text-transform: uppercase;
+  background: linear-gradient(90deg, #ff5555, #0000ff);
+  font-weight: bold;
+  font-size: 8vw;
+  color: #000;
+  background-clip: text;
+  text-fill-color: transparent;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`
+
+const BorrowButton = styled(Button)({
+  border: 'none',
+  fontWeight: 'bold',
+  background: 'transparent',
+  fontSize: '2vw',
+  textTransform: 'uppercase',
+})
