1a4cae6c50d65705b30499f51e46475afb9b7288
Adam Azad <adam@adamazad.com>
2021-02-05 14:58:12 +0300

    Add Leverage view

---
+++ src/views/Leverage/index.tsx
@@ -0,0 +1,68 @@
+// Externals
+import styled from 'styled-components'
+import React, { useEffect } from 'react'
+
+// Hooks
+import { useOpenSeaAssets } from 'src/hooks/useOpenSeaAssets'
+
+// Layouts
+import { HeaderAndContent as Layout } from 'src/layouts/HeaderAndContent'
+import { Center } from 'src/layouts/Center'
+
+// Components
+import { NFTAssetCard } from './components/NFTAssetCard'
+import { Container } from 'src/components/Container'
+
+const NoAssetsMessage = () => <Center>Hmmm ... you do not own any asset</Center>
+
+export function LeverageView() {
+  const { assets, loading } = useOpenSeaAssets()
+
+  if (loading) {
+    return (
+      <Layout>
+        <Container>Finding NFTs</Container>
+      </Layout>
+    )
+  }
+
+  // No assets
+  if (assets.length === 0) {
+    return (
+      <Layout>
+        <Container>
+          <NoAssetsMessage />
+        </Container>
+      </Layout>
+    )
+  }
+
+  console.log(assets)
+
+  return (
+    <Layout>
+      <Container>
+        <HeaderText>Leverage</HeaderText>
+        <CardGrid>
+          {assets.map(userAsset => {
+            const key = `${userAsset.tokenId}-${userAsset.assetContract}`
+            return <NFTAssetCard asset={userAsset} key={key} />
+          })}
+        </CardGrid>
+      </Container>
+    </Layout>
+  )
+}
+
+const CardGrid = styled.div(props => ({
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gridTemplateRows: 'repeat(3, 1fr)',
+}))
+
+const HeaderText = styled.h1({
+  textAlign: 'center',
+  fontSize: '6vw',
+  fontWeight: 'bold',
+  textTransform: 'uppercase',
+})
