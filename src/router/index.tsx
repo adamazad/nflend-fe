f62db9943bbd1e6842aca9011ed2ba8c9b5d1981
Adam Azad <adam@adamazad.com>
2021-02-06 11:04:11 +0300

    Update routes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,25 +1,23 @@
-// External
+// Externals
 import { Route, Switch } from 'react-router-dom'
 
 // Views
 import { CreateBorrowRequestView } from 'src/views/CreateBorrowRequest'
 import { BorrowRequestsView } from 'src/views/BorrowRequests'
-import { DepisteNFTView } from 'src/views/DepositNFT'
+import { BorrowRequestView } from 'src/views/BorrowRequest'
+import { LeverageView } from 'src/views/Leverage'
 import { NotFoundView } from 'src/views/NotFound'
-import { ConnectView } from 'src/views/Connect'
 import { LandingView } from 'src/views/Landing'
-import { BorrowView } from 'src/views/Borrow'
-import { LendView } from 'src/views/Lend'
+import { EarnView } from 'src/views/Earn'
 
 export const AppRouter = () => {
   return (
     <Switch>
-      <Route exact path="/connect" component={ConnectView} />
-      <Route exact path="/borrow" component={BorrowView} />
-      <Route exact path="/lend" component={LendView} />
-      <Route exact path="/assets/deposit" component={DepisteNFTView} />
+      <Route exact path="/lend" component={EarnView} />
+      <Route exact path="/leverage" component={LeverageView} />
       <Route exact path="/borrow-requests" component={BorrowRequestsView} />
       <Route exact path="/borrow-requests/new" component={CreateBorrowRequestView} />
+      <Route exact path="/borrow-requests/:borrowRequestId" component={BorrowRequestView} />
       <Route exact path="/" component={LandingView} />
       <Route exact path="*" component={NotFoundView} />
     </Switch>
