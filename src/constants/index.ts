9adfd09661db4d9a5c96781baac0bfe71dfa4af0
Adam Azad <adam@adamazad.com>
2021-02-06 13:01:58 +0300

    Add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,11 @@
+export const APP_NAME = process.env.REACT_APP_APP_NAME || 'React App'
+export const INFURA_API_KEY = process.env.REACT_APP_INFURA_API_KEY || undefined
+
+export const LOAN_MANAGER_ADDRESS = process.env.LOAN_MANAGER_ADDRESS || '0x7bad4cd72aac2f879423e5abd2dcfb26aa35b7c4'
+
+// Popular contract addresses
+export const ENS_CONTRACT_ADDRESS = ''
+export const CRYPTOKITTIES_CONTRACT_ADDRESS = ''
+export const AAVEGOTCHI_CONTRACT_ADDRESS = ''
+export const SORARE_CONTRACT_ADDRESS = ''
+export const RARIBLE_CONTRACT_ADDRESS = ''
