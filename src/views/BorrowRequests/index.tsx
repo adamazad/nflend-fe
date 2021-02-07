4892c7000ea3f9d301f0e5dbef812f9237a8c880
Adam Azad <adam@adamazad.com>
2021-02-08 00:28:52 +0300

    Earn view

--- src/views/BorrowRequests/index.tsx
+++ src/views/BorrowRequests/index.tsx
@@ -7,11 +7,13 @@ import { useSetPageTitle } from 'src/hooks/useSetPageTitle'
 
 // Components
 import { BorrowRequestCard } from './components/BorrowRequestCard'
+import { ErrorMesssage } from 'src/components/ErrorMessage'
 import { Container } from 'src/components/Container'
 
 // Layouts
 import { HeaderAndContent as Layout } from 'src/layouts/HeaderAndContent'
 import { Center } from 'src/layouts/Center'
+import styled from 'styled-components'
 
 export function BorrowRequestsView() {
   const { borrowRequests, error, loading } = useBorrowRequests()
@@ -25,7 +27,19 @@ export function BorrowRequestsView() {
     return (
       <Layout>
         <Center minHeight="100%">
-          <Container>Loading!</Container>
+          <Container>Loading</Container>
+        </Center>
+      </Layout>
+    )
+  }
+
+  if (error) {
+    return (
+      <Layout>
+        <Center minHeight="100%">
+          <Container>
+            <ErrorMesssage error={error} />
+          </Container>
         </Center>
       </Layout>
     )
@@ -33,16 +47,35 @@ export function BorrowRequestsView() {
 
   return (
     <Layout>
-      <Center minHeight="100%">
+      <Center minHeight="100%" minWidth="100%">
         <Container>
-          {borrowRequests.map(borrowRequest => (
-            <BorrowRequestCard
-              borrowRequest={borrowRequest}
-              key={`${borrowRequest.nftId}-${borrowRequest.cancelTimestamp}`}
-            />
-          ))}
+          <CardGrid>
+            {borrowRequests.map(borrowRequest => (
+              <BorrowRequestCard
+                borrowRequest={borrowRequest}
+                key={`${borrowRequest.nftId}-${borrowRequest.cancelTimestamp}`}
+              />
+            ))}
+          </CardGrid>
         </Container>
       </Center>
     </Layout>
   )
 }
+
+const CardGrid = styled.div(
+  props => ({
+    display: 'grid',
+    gridTemplateColumns: 'repeat(1, 1fr)',
+    gridTemplateRows: 'repeat(3, 1fr)',
+    gap: props.theme.space[3],
+  }),
+  props => `
+    @media (min-width: ${props.theme.breakpoints[1]}) {
+      grid-template-columns: repeat(auto-fit, minmax(240px, 2fr));
+    }
+    @media (min-width: ${props.theme.breakpoints[2]}) {
+      grid-template-columns: repeat(auto-fit, minmax(240px, 3fr));
+    }
+  `
+)
