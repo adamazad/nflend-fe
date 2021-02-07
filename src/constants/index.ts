045c764ecfddd3fea7810401e4b13330a4844b16
Adam Azad <adam@adamazad.com>
2021-02-07 19:50:04 +0300

    Add Kovan addresses

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,10 @@
 export const APP_NAME = process.env.REACT_APP_APP_NAME || 'React App'
 export const INFURA_API_KEY = process.env.REACT_APP_INFURA_API_KEY || undefined
 
-export const LOAN_MANAGER_ADDRESS = process.env.LOAN_MANAGER_ADDRESS || '0x7bad4cd72aac2f879423e5abd2dcfb26aa35b7c4'
+export const LOAN_MANAGER_ADDRESS = process.env.LOAN_MANAGER_ADDRESS || '0x1c733fc1dc10d395b8e19babe6fc949a52b91fcd'
+
+// Mock Contract address
+export const MOCK_NFT_CONTRACT_ADDRESS = '0x6ccdaa678575e91324cc9acf37818e02103977f3'
 
 // Popular contract addresses
 export const ENS_CONTRACT_ADDRESS = ''
