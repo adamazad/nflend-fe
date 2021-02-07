9d9140c38c22e9ef520f56ccaf3004c743e7b5af
Adam Azad <adam@adamazad.com>
2021-02-07 21:49:34 +0300

    Fetch only user's NFTs

--- src/hooks/useAccountAssets.ts
+++ src/hooks/useAccountAssets.ts
@@ -1,9 +1,19 @@
+import { useCallback, useEffect, useState } from 'react'
 import { Provider } from '@ethersproject/providers'
 import { OpenSeaAsset } from 'opensea-js/lib/types'
 import { useWeb3React } from '@web3-react/core'
-import { useCallback, useEffect, useState } from 'react'
+import Axios from 'axios'
 
+// Helpers
+import { mapFakeAavegotchiToOpenSeaAsset } from 'src/contracts/helpers'
+
+import { MOCK_NFT_CONTRACT_ADDRESS } from 'src/constants'
+
+// Contract Facotories
 import { ERC721Factory } from 'src/contracts'
+
+// Interfaces
+import { TokenMetadata } from 'src/interfaces/TokenMetadata'
 
 interface UseAccountAssetsReturn {
   error: Error | false
@@ -11,8 +21,21 @@ interface UseAccountAssetsReturn {
   assets: OpenSeaAsset[]
 }
 
-// Mock Contract address
-const MOCK_NFT_CONTRACT_ADDRESS = '0xe135941f95120810614d05129b84d63a88605641'
+const tokenURICacheMap = new Map<string, TokenMetadata>()
+
+async function fetchTokenURIData(url: string) {
+  // Retrieve from cache
+  if (tokenURICacheMap.has(url)) {
+    return tokenURICacheMap.get(url) as TokenMetadata
+  }
+
+  const { data } = await Axios.get<TokenMetadata>(url)
+
+  // Cache
+  tokenURICacheMap.set(url, data)
+
+  return data
+}
 
 export function useAccountAssets(account: string): UseAccountAssetsReturn {
   const [loading, setLoading] = useState<boolean>(true)
@@ -26,26 +49,35 @@ export function useAccountAssets(account: string): UseAccountAssetsReturn {
 
       const erc721Contract = ERC721Factory.connect(MOCK_NFT_CONTRACT_ADDRESS, library)
 
-      console.log(erc721Contract)
-
       try {
-        const tokenIds = await erc721Contract.getOwnerTokens(account)
+        const totalSupply = (await erc721Contract.totalSupply()).toNumber()
 
-        console.log(tokenIds)
+        const assets: OpenSeaAsset[] = []
 
-        tokenIds.forEach(async token => {
-          const tokenMetadata = await erc721Contract.tokenMetadata(token)
+        for (let index = 0; index <= totalSupply; index++) {
+          try {
+            const ownerOfToken = await erc721Contract.ownerOf(index)
 
-          console.log(tokenMetadata)
-        })
-      } catch (e) {}
+            if (ownerOfToken === account) {
+              const tokenURI = await erc721Contract.tokenURI(index)
+              const data = await fetchTokenURIData(tokenURI)
+              assets.push(mapFakeAavegotchiToOpenSeaAsset(data, index.toString()))
+            }
+          } catch (e) {}
+        }
+
+        setAssets(assets)
+      } catch (e) {
+        setError(e)
+      }
+      setLoading(false)
     },
     [library]
   )
 
   useEffect(() => {
     fetchAssets(account)
-  }, [fetchAssets, account])
+  }, [fetchAssets, account, library])
 
   return {
     error,
